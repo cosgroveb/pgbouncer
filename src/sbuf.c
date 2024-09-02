cd7a4fd6100c8f6c99c6d22a5c29d1f58bd5e9c5
Brian Cosgrove <bcosgrove@paypal.com>
2024-09-02 22:59:08 +0000

    refactor

--- src/sbuf.c
+++ src/sbuf.c
@@ -1236,6 +1236,27 @@ static bool setup_tls(struct tls_config *conf, const char *pfx, int sslmode,
 	return true;
 }
 
+static bool tls_config_changed(struct tls_config *new_server_connect_conf)
+{
+  return (strings_equal(new_server_connect_conf[0].ca_file, server_connect_conf->ca_file) &&
+    strings_equal(new_server_connect_conf[0].ca_path, server_connect_conf->ca_path) &&
+    strings_equal(new_server_connect_conf[0].ca_mem, server_connect_conf->ca_mem) &&
+    (new_server_connect_conf[0].ca_len == server_connect_conf->ca_len) &&
+    strings_equal(new_server_connect_conf[0].ciphers, server_connect_conf->ciphers) &&
+    (new_server_connect_conf[0].ciphers_server == server_connect_conf->ciphers_server) &&
+    (new_server_connect_conf[0].dheparams == server_connect_conf->dheparams) &&
+    (new_server_connect_conf[0].ecdhecurve == server_connect_conf->ecdhecurve) &&
+    strings_equal(new_server_connect_conf[0].ocsp_file, server_connect_conf->ocsp_file) &&
+    strings_equal(new_server_connect_conf[0].ocsp_mem, server_connect_conf->ocsp_mem) &&
+    (new_server_connect_conf[0].ocsp_len == server_connect_conf->ocsp_len) &&
+    (new_server_connect_conf[0].protocols == server_connect_conf->protocols) &&
+    (new_server_connect_conf[0].verify_cert == server_connect_conf->verify_cert) &&
+    (new_server_connect_conf[0].verify_client == server_connect_conf->verify_client) &&
+    (new_server_connect_conf[0].verify_depth == server_connect_conf->verify_depth) &&
+    (new_server_connect_conf[0].verify_name == server_connect_conf->verify_name) &&
+    (new_server_connect_conf[0].verify_time == server_connect_conf->verify_time));
+}
+
 static bool skip_tag_pools_dirty(struct tls_config *new_server_connect_conf)
 {
   if (server_connect_sslmode != cf_server_tls_sslmode) {
@@ -1245,24 +1266,7 @@ static bool skip_tag_pools_dirty(struct tls_config *new_server_connect_conf)
     log_debug("no existing server ssl config detected. marking pools dirty.");
     return false;
   }
-  else if (
-      strings_equal(new_server_connect_conf[0].ca_file, server_connect_conf->ca_file) &&
-      strings_equal(new_server_connect_conf[0].ca_path, server_connect_conf->ca_path) &&
-      strings_equal(new_server_connect_conf[0].ca_mem, server_connect_conf->ca_mem) &&
-      (new_server_connect_conf[0].ca_len == server_connect_conf->ca_len) &&
-      strings_equal(new_server_connect_conf[0].ciphers, server_connect_conf->ciphers) &&
-      (new_server_connect_conf[0].ciphers_server == server_connect_conf->ciphers_server) &&
-      (new_server_connect_conf[0].dheparams == server_connect_conf->dheparams) &&
-      (new_server_connect_conf[0].ecdhecurve == server_connect_conf->ecdhecurve) &&
-      strings_equal(new_server_connect_conf[0].ocsp_file, server_connect_conf->ocsp_file) &&
-      strings_equal(new_server_connect_conf[0].ocsp_mem, server_connect_conf->ocsp_mem) &&
-      (new_server_connect_conf[0].ocsp_len == server_connect_conf->ocsp_len) &&
-      (new_server_connect_conf[0].protocols == server_connect_conf->protocols) &&
-      (new_server_connect_conf[0].verify_cert == server_connect_conf->verify_cert) &&
-      (new_server_connect_conf[0].verify_client == server_connect_conf->verify_client) &&
-      (new_server_connect_conf[0].verify_depth == server_connect_conf->verify_depth) &&
-      (new_server_connect_conf[0].verify_name == server_connect_conf->verify_name) &&
-      (new_server_connect_conf[0].verify_time == server_connect_conf->verify_time)) {
+  else if (tls_config_changed(new_server_connect_conf)) {
     log_debug("no server ssl config change detected. not marking pools dirty.");
     return true;
   } else {
