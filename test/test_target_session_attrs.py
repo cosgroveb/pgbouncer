5c11cfc2f5d8c59e290ab6019a1d13308e625f1a
Brian Cosgrove <cosgroveb@gmail.com>
2025-03-03 14:18:53 +0000

    changing the server login retry passes these four tests and breaks the valgrind build
    Co-authored-by: Brian Cosgrove <bcosgrove@paypal.com>

--- test/test_target_session_attrs.py
+++ test/test_target_session_attrs.py
@@ -18,6 +18,13 @@ def skip_if_macos_and_no_sudo():
         yield
 
 
+@pytest.fixture(autouse=True)
+def setup_test_target_session_attrs(bouncer):
+    bouncer.admin(f"set server_login_retry=1")
+    yield
+    bouncer.admin(f"set server_login_retry=15")
+
+
 @pytest.fixture
 def bouncer_transaction_read_only(bouncer):
     bouncer.pg.psql("ALTER DATABASE p0 SET default_transaction_read_only=on")
@@ -32,7 +39,6 @@ def test_target_session_attrs_primary_first(bouncer, replica):
 
 @pytest.mark.timeout(62)
 def test_target_session_attrs_primary_second(bouncer, replica):
-    #bouncer.admin(f"set server_login_retry=1")
     with bouncer.log_contains(
         r"127.0.0.2:\d+ closing because: server does not satisfy target_session_attrs",
         1,
@@ -47,7 +53,6 @@ def test_target_session_attrs_standby_first(bouncer, replica):
 
 @pytest.mark.timeout(62)
 def test_target_session_attrs_standby_second(bouncer, replica):
-    #bouncer.admin(f"set server_login_retry=1")
     with bouncer.log_contains(
         r"127.0.0.1:\d+ closing because: server does not satisfy target_session_attrs",
         1,
@@ -62,7 +67,6 @@ def test_target_session_attrs_readonly_first(bouncer, replica):
 
 @pytest.mark.timeout(62)
 def test_target_session_attrs_readonly_second(bouncer, replica):
-    #bouncer.admin(f"set server_login_retry=1")
     with bouncer.log_contains(
         r"127.0.0.1:\d+ closing because: server does not satisfy target_session_attrs",
         1,
@@ -87,7 +91,6 @@ def test_target_session_attrs_readwrite_first(bouncer, replica):
 
 @pytest.mark.timeout(62)
 def test_target_session_attrs_readwrite_second(bouncer, replica):
-    #bouncer.admin(f"set server_login_retry=1")
     with bouncer.log_contains(
         r"127.0.0.2:\d+ closing because: server does not satisfy target_session_attrs",
         1,
